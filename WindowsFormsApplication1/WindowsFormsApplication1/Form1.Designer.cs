6de62e4d995f1984861b61b808197f73e34d9725
Roman459 <tagirov@360@mail.ru>
2015-11-30 16:45:15 +0600

    nf1

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode55 = new System.Windows.Forms.TreeNode("BranchNode");
+            System.Windows.Forms.TreeNode treeNode56 = new System.Windows.Forms.TreeNode("leafNode");
+            System.Windows.Forms.TreeNode treeNode57 = new System.Windows.Forms.TreeNode("NODE1", new System.Windows.Forms.TreeNode[] {
+            treeNode55,
+            treeNode56});
+            System.Windows.Forms.TreeNode treeNode58 = new System.Windows.Forms.TreeNode("BranchNode");
+            System.Windows.Forms.TreeNode treeNode59 = new System.Windows.Forms.TreeNode("leafNode");
+            System.Windows.Forms.TreeNode treeNode60 = new System.Windows.Forms.TreeNode("NODE2", new System.Windows.Forms.TreeNode[] {
+            treeNode58,
+            treeNode59});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            treeNode55.Name = "BranchNode";
+            treeNode55.Text = "BranchNode";
+            treeNode56.Name = "leafNode";
+            treeNode56.Text = "leafNode";
+            treeNode57.Name = "NODE1";
+            treeNode57.Text = "NODE1";
+            treeNode58.Name = "BranchNode";
+            treeNode58.Text = "BranchNode";
+            treeNode59.Name = "leafNode";
+            treeNode59.Text = "leafNode";
+            treeNode60.Name = "NODE2";
+            treeNode60.Text = "NODE2";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode57,
+            treeNode60});
+            this.treeView1.Size = new System.Drawing.Size(313, 477);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            this.treeView1.NodeMouseHover += new System.Windows.Forms.TreeNodeMouseHoverEventHandler(this.treeView1_NodeMouseHover);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Добавить корень";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(331, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(381, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 34);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Удалить выделенный узел";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 160);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Переименовать узел";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(478, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 34);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Добавить ветвь";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 14);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(267, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(331, 209);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(293, 106);
+            this.panel1.TabIndex = 8;
+            this.panel1.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(70, 50);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 34);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Применить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(454, 379);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 31);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Дерево из файла";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text files (*.txt) | *.txt";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(331, 379);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 31);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Дерево в файл";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "Text files (*.txt) | *.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 477);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Работа с NODES";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView1.Nodes.Add(textBox1.Text);
+           // treeView1.Nodes[1].Nodes.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Remove();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //treeView1.Nodes.Clear();
+            //textBox1.Text = treeView1.Nodes.IndexOfKey;
+           // treeView1.Nodes.IndexOf() = label1.Text;
+           // TreeNode n = treeView1.SelectedNode;
+             //label1.Text = Convert.ToString(n);
+            //textBox2.AppendText(Convert.ToString(n));
+           // treeView1.Nodes[0].Text = textBox2.Text;
+            //treeView1.SelectedNode.Text = textBox2.Text;
+            panel1.Visible = true;
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Nodes.Add(textBox1.Text);
+            
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            textBox2.Clear();
+           textBox2.AppendText(e.Node.Text);
+            
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            
+        }
+
+        private void treeView1_NodeMouseHover(object sender, TreeNodeMouseHoverEventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            treeView1.SelectedNode.Text = textBox2.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+
+            openFileDialog1.ShowDialog();
+            string fileName = openFileDialog1.FileName;
+
+            treeView1.Nodes.Clear();
+
+            using (Stream file = File.Open(fileName, FileMode.Open))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                object obj = bf.Deserialize(file);
+
+                TreeNode[] nodeList = (obj as IEnumerable<TreeNode>).ToArray();
+                treeView1.Nodes.AddRange(nodeList);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            string fileName = saveFileDialog1.FileName;
+            
+            using (Stream file = File.Open(fileName, FileMode.Create))
+            {
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(file, treeView1.Nodes.Cast<TreeNode>().ToList());
+            }
+        }
+
+       
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+    }
+}
